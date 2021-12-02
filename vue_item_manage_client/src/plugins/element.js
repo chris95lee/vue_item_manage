b3ed15e66376ce5bbb34d24fad3c599dd8f07531
chrislee <lh6649298@163.com>
2021-12-03 00:50:40 +0800

    main begins

--- vue_item_manage_client/src/plugins/element.js
+++ vue_item_manage_client/src/plugins/element.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Form, FormItem, Input, Button, Message, Container, Header, Aside, Main } from 'element-ui'
+import { Form, FormItem, Input, Button, Message, Container, Header, Aside, Main, Menu, Submenu, MenuItem } from 'element-ui'
 
 Vue.use(Form)
 Vue.use(FormItem)
@@ -9,4 +9,7 @@ Vue.use(Container)
 Vue.use(Header)
 Vue.use(Aside)
 Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
 Vue.prototype.$message = Message
