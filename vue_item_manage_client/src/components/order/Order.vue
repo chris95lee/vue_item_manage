21ecbd57ee6e00ef00b3f62e1b131280cd56cdf8
chrislee <lh6649298@163.com>
2022-01-14 00:55:14 +0800

    just go

--- vue_item_manage_client/src/components/order/Order.vue
+++ vue_item_manage_client/src/components/order/Order.vue
@@ -20,19 +20,48 @@
         <el-table-column type="index" label="#"></el-table-column>
         <el-table-column label="订单编号" prop="order_number"></el-table-column>
         <el-table-column label="订单价格" prop="order_price"></el-table-column>
-        <el-table-column label="是否付款"></el-table-column>
+        <el-table-column label="是否付款" prop="pay_status">
+          <template v-slot="scope">
+            <el-tag v-if="scope.row.pay_status === '0'" type="danger">未付款</el-tag>
+            <el-tag v-else type="success">已付款</el-tag>
+          </template>
+        </el-table-column>
         <el-table-column label="是否发货" prop="is_send"></el-table-column>
-        <el-table-column label="下单时间" prop="create_time"></el-table-column>
-        <el-table-column label="操作"></el-table-column>
+        <el-table-column label="下单时间" prop="create_time">
+          <template v-slot="scope">{{dateFormat(scope.row.create_time)}}</template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template>
+            <el-button type="primary" icon="el-icon-edit" size="mini" @click="showEditBox"></el-button>
+            <el-button type="success" icon="el-icon-location" size="mini"></el-button>
+          </template>
+        </el-table-column>
       </el-table>
       <!-- 分页 -->
-      <el-pagination layout="total, sizes, prev, pager, next, jumper" :total="total">
+      <el-pagination @size-change="handleSizeChange"
+      @current-change="handleCurrentChange" :current-page="queryInfo.pagenum" :page-sizes="[5, 10, 15]" :page-size="queryInfo.pagesize" layout="total, sizes, prev, pager, next, jumper" :total="total">
       </el-pagination>
     </el-card>
+    <!-- 修改dialog -->
+    <el-dialog title="修改地址" :visible.sync="editBoxVisible" width="50%" @close="editFormClose">
+      <el-form :model="editForm" :rules="editFormRules" ref="editFormRef" label-width="100px">
+        <el-form-item label="省市区/县" prop="address1">
+          <el-cascader v-model="editForm.address1" :options="cityData" :props="editFormProps"></el-cascader>
+        </el-form-item>
+        <el-form-item label="详细地址" prop="address2">
+          <el-input v-model="editForm.address2"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="editBoxVisible = false">取 消</el-button>
+        <el-button type="primary" @click="editBoxVisible = false">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
+import cityData from './citydata.js'
 export default {
   data () {
     return {
@@ -42,7 +71,24 @@ export default {
         pagenum: 1
       },
       orderList: [],
-      total: 0
+      total: 0,
+      editBoxVisible: false,
+      editForm: {
+        address1: [],
+        address2: ''
+      },
+      editFormRules: {
+        address1: [
+          { required: true, message: '请选择省市区/县', trigger: 'blur' }
+        ],
+        address2: [
+          { required: true, message: '请填写详细地址', trigger: 'blur' }
+        ]
+      },
+      editFormProps: {
+        expandTrigger: 'hover'
+      },
+      cityData
     }
   },
   created () {
@@ -59,6 +105,32 @@ export default {
       }
       this.orderList = res.data.goods
       this.total = res.data.total
+    },
+    dateFormat (originVal) {
+      const dt = new Date(originVal)
+      const y = dt.getFullYear()
+      const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+      const d = (dt.getDate() + '').padStart(2, '0')
+      const hh = (dt.getHours() + '').padStart(2, '0')
+      const mm = (dt.getMinutes() + '').padStart(2, '0')
+      const ss = (dt.getSeconds() + '').padStart(2, '0')
+      return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+    },
+    // 分页
+    handleSizeChange (newSize) {
+      this.queryInfo.pagesize = newSize
+      this.getOrderList()
+    },
+    handleCurrentChange (nowPage) {
+      this.queryInfo.pagenum = nowPage
+      this.getOrderList()
+    },
+    // 修改
+    showEditBox () {
+      this.editBoxVisible = true
+    },
+    editFormClose () {
+      this.$refs.editFormRef.resetFields()
     }
   },
   computed: {
@@ -68,5 +140,7 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+  .el-cascader {
+    width: 100%;
+  }
 </style>
